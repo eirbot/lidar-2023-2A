aa5b2cacf4e887a97d2b45ef00fe814de9421174
leov66 <leo.v66.lov@gmail.com>
2023-05-11 19:27:52 +0200

    ready

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "mbed.h"
 #include "constantes.h"
-#include "Lidar.h"
+ #include "Lidar.h"
 
 DigitalOut led(LED1);
 BufferedSerial lidarUART(TX_Lidar, RX_Lidar, LIDAR_BAUD_RATE);
@@ -8,7 +8,7 @@ PwmOut pwm(PWM);
 DigitalOut avertissement(GPIO_Avertissement);
 DigitalOut danger(GPIO_Danger);
 
-Lidar lidar(&lidarUART, 200);
+Lidar lidar(&lidarUART, 300);
 
 /*
  * Utiliser printf pour envoyer à l'ordinateur des données à 115200 bauds/s
@@ -24,12 +24,14 @@ int main(){
     led = 0;
 
     lidar.init();
-
+    ;
+    
     // Loop
     while(1){
-        led = 1;
-        ThisThread::sleep_for(1s);
-        led = 0;
-        ThisThread::sleep_for(1s);
+//        led = 1;
+//        ThisThread::sleep_for(1s);
+//        led = 0;
+//        ThisThread::sleep_for(1s);
+        lidar.position();
     }
 }
