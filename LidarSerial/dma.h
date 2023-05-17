d40d21d7ca4adcee49269ae01c9a9e4883a8bb0f
leov66 <leo.v66.lov@gmail.com>
2023-05-17 19:56:43 +0200

    add dma config from cube mx little modified

---
+++ LidarSerial/dma.h
@@ -0,0 +1,26 @@
+#ifndef __DMA_H__
+#define __DMA_H__
+
+/* Includes ------------------------------------------------------------------*/
+#include "mbed.h"
+
+
+/* DMA memory to memory transfer handles -------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+void MX_DMA_Init();
+
+/* USER CODE BEGIN Prototypes */
+
+/* USER CODE END Prototypes */
+
+
+#endif /* __DMA_H__ */
+
